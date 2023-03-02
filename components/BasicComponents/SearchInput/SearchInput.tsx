f2222f373da4613cc90e9439882397a639d5ce9d
Erik Sabin <eriksabin97@gmail.com>
2023-03-01 22:02:09 -0500

    Added more props and started datalist

--- components/BasicComponents/SearchInput/SearchInput.tsx
+++ components/BasicComponents/SearchInput/SearchInput.tsx
@@ -1,21 +1,43 @@
+import styles from './SearchInput.module.scss'
+
+
+interface DataListProps {
+    /*Value for data list option */
+    value: string | number | readonly string[] | undefined
+}
 
 interface SearchInputProps {
+    /*PlaceHolder text for input */
     placeholder?: string;
+    /*Id for input */
     id?: string;
+    /*Id for data list */
+    dataListId?: string;
+    /*Name for input */
     name?: string;
+    /*Toggle required state for input */
     required: boolean;
+    /*Toggle Error state for input */
     error: boolean;
+    /*Text underneath the input */
     helperText: string;
+    /*Accepts an change event function for input */
     onChange:(evt: React.ChangeEvent<HTMLInputElement>) => void;
+    /*List of options for the Data list */
+    data: DataListProps[]
 }
+
 
 export default function SearchInput(props: SearchInputProps) {
 
-    const {placeholder, required, id, name, error, onChange} = props
+    const {placeholder, required, id, name, error, onChange, data, dataListId, helperText} = props
+
+    const inputStyles = [styles.inputContainer]
+
+    if (error) inputStyles.push(styles.error)
 
     return (
-        <div>
-            
+        <div className={inputStyles.join(' ')}>
             <input
                 placeholder={placeholder}
                 required={required}
@@ -23,9 +45,14 @@ export default function SearchInput(props: SearchInputProps) {
                 name={name}
                 type='search'
                 onChange={onChange}
+                className={styles.input}
             />
-            <div className={styles.list}></div>
-            <label className={styles.helperText}>{helperText}</label>
+            {data && <datalist className={styles.dataList} id={dataListId}>
+                {data.map((item) => 
+                    <option value={item.value}/>
+                )}
+            </datalist>}
+            {helperText && <label className={styles.helperText}>{helperText}</label>}
         </div>
     )
 }
