01833c3ae07f21a1fb3b2f9c9d98c77ad9d02fc3
Erik Sabin <eriksabin97@gmail.com>
2023-03-11 10:18:25 -0500

    added more styles and started switch component

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,8 +1,12 @@
 import Notification from "@/components/PageComponents/Notification/Notification";
+import styles from './NotificationPage.module.scss';
 
 export default function NotificationPage() {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>Notifications</h1>
+            </div>
             <Notification />
         </div>
     )
---
+++ components/BasicComponents/Switch/Switch.tsx
@@ -0,0 +1,9 @@
+import styles from './Switch.module.scss';
+
+export default function Switch() {
+
+    <div className={styles.switch}>
+        <input type='checkbox'/>
+        <div className={styles.slider}></div>
+    </div>
+}
