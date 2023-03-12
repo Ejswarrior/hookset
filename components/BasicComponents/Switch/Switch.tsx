d06c4ac673d567b34eb0016aad65059b8f195d74
Erik Sabin <eriksabin97@gmail.com>
2023-03-12 16:59:48 -0400

    added changes

--- components/BasicComponents/Switch/Switch.tsx
+++ components/BasicComponents/Switch/Switch.tsx
@@ -1,5 +1,11 @@
 import styles from './Switch.module.scss';
 
+interface SwitchProps {
+    checked: boolean;
+    disabled: boolean;
+    
+}
+
 export default function Switch() {
 
     <div className={styles.switch}>
