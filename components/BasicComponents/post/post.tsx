da050886b102adf7f5da2d5880358ca2dfbe5821
Erik Sabin <eriksabin97@gmail.com>
2023-05-21 13:36:13 -0400

    feat: added Tab component and started fishopedia page component

--- components/BasicComponents/post/post.tsx
+++ components/BasicComponents/post/post.tsx
@@ -1,25 +1,26 @@
-import styles from "./Post.module.scss";
-import Profile from "../Profile/Profile";
+import styles from './Post.module.scss';
+import Profile from '../Profile/Profile';
+
 
 export default function Post() {
 
-	return(
-		<div className={styles.postContainer}>
-			<div className={styles.attribution}>
-				<div className={styles.postProfile}>
-					<Profile expanded={false}/>
-					<label className={styles.postUsername}>Username</label>
-				</div>
-				<button className={styles.postSettings}>...</button>
-			</div>
+    return(
+    <div className={styles.postContainer}>
+        <div className={styles.attribution}>
+            <div className={styles.postProfile}>
+                <Profile expanded={false}/>
+                <label className={styles.postUsername}>Username</label>
+            </div>
+            <button className={styles.postSettings}>...</button>
+        </div>
 
-			<span className={styles.postContent}>I went for a walk today and found that I really hate winter. Like why is it always so cold</span>
-			<div className={styles.postIcons}>
-				<p>0</p>
-				<button className={styles.buttonComment}><i className={styles.iconComment}/></button>
-				<p>0</p>
-				<button className={styles.buttonLike}><i className={styles.iconLike}/></button>
-			</div>
-		</div>
-	)
+        <span className={styles.postContent}>I went for a walk today and found that I really hate winter. Like why is it always so cold</span>
+        <div className={styles.postIcons}>
+            <p>0</p>
+            <button className={styles.buttonComment}><i className={styles.iconComment}/></button>
+            <p>0</p>
+            <button className={styles.buttonLike}><i className={styles.iconLike}/></button>
+        </div>
+    </div>
+    )
 }
