ae17f42919de69a58e7e4315eefc0460e020b6cf
Erik Sabin <eriksabin97@gmail.com>
2023-07-11 22:28:06 -0400

    feat: added router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express'
+import { config } from 'dotenv'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+const processEnv = config().parsed;
 
 app.get('/hi' , (req: Request, res: Response) => {
     console.log('/hi')
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/post', router)
+
+app.listen(processEnv.PORT, () => {
+    console.log('server running')
 })
